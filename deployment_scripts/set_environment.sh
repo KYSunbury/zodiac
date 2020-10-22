e917f480504d60d6940bb9e795779f18dd1a4c77
KYSunbury <73312045+KYSunbury@users.noreply.github.com>
2020-10-22 21:25:59 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=bpsirmione
-export LIMONE_ALIAS=bplimone
-export SCORPIO_ALIAS=bpscorpio
-export VIRGO_ALIAS=bpvirgo
-export LIBRA_ALIAS=bplibra
-export ZODIAC_ALIAS=bpzodiac
-export DB_ADMIN_USER=nick
+export SIRMIONE_ALIAS=ky4211sirmione
+export LIMONE_ALIAS=ky4211limone
+export SCORPIO_ALIAS=ky4211scorpio
+export VIRGO_ALIAS=ky4211virgo
+export LIBRA_ALIAS=ky4211libra
+export ZODIAC_ALIAS=ky4211zodiac
+export DB_ADMIN_USER=kaan
 export AAD_DOMAIN=xekina.onmicrosoft.com
-export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
-export AAD_CLIENTID=ccee7608-940c-42d9-ba86-a2845ef3a808
+export AAD_TENANTID=ea80952e-a476-42d4-aaf4-5457852b0f7e
+export AAD_CLIENTID=e2b0dd40-1203-45ef-b812-d1fd6f630f5f
 export OUTPUT=json
 
 # Whatever you set zodiac instance to will be tagged onto your azure resources, and enable you to access all the different components as a cohesive set
-export ZODIAC_INSTANCE=Jemima
+export ZODIAC_INSTANCE=HelloDolly
 
 echo "<h2>Environment Variables</h2>" >> deployment-log.html
 echo "<p>ZODIAC_INSTANCE:$ZODIAC_INSTANCE</p>" >> deployment-log.html
